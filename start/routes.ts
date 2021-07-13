3b359aae668688f7b27541326ff87a8530e5d1fb
«Misha» <bolotnikovms@yandex.ru>
2021-07-13 21:18:01 +0500

    Updated files

--- start/routes.ts
+++ start/routes.ts
@@ -22,7 +22,7 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/', async ({ view }) => {
   return view.render('pages/index')
-})
+}).as('index')
 
 Route.group(() => {
   Route.get('/', 'ProgramListsController.index').as('index.program')
