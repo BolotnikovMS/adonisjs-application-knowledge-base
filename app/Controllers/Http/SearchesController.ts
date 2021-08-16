6be720079b79d87366dd7ae517e851a992fa4dba
Mikhail <bolotnikovms@yandex.ru>
2021-08-16 19:18:08 +0500

    Revert "Updated files"

--- app/Controllers/Http/SearchesController.ts
+++ app/Controllers/Http/SearchesController.ts
@@ -6,49 +6,73 @@ import Question from 'App/Models/Question'
 import RequestSearchValidator from 'App/Validators/RequestSearchValidator'
 
 export default class SearchesController {
-  public async searchInProgram({ request, view }: HttpContextContract) {
-    const validateData = await request.validate(RequestSearchValidator)
+  public async searchInProgram({ request, response, session, view }: HttpContextContract) {
+    const validatedData = await request.validate(RequestSearchValidator)
 
-    const search = validateData.search.split(' ')
-    let searchResult
-
-    if (search.length >= 1) {
-      searchResult = await ProgramList.query()
-        .where((query) => {
-          query
-            .where('name', 'like', `%${validateData.search}%`)
-            .orWhere('name', 'like', `%${validateData.search}`)
-            .orWhere('name', 'like', `${validateData.search}%`)
-        })
+    for (const validatedDataKey in validatedData) {
+      if (!validatedData[validatedDataKey]) {
+        delete validatedData[validatedDataKey]
+      }
     }
 
-    return view.render('pages/programs/search', {
-      title: 'Результаты поиска',
-      searchResult,
-      searchStr: validateData.search
-    })
+    if (Object.keys(validatedData).length === 0) {
+      session.flash('dangermessage', `Введенно пустое значение в поле название.`)
+      response.redirect('/list-program/')
+    } else {
+      if (validatedData.hasOwnProperty('search')) {
+        const search = validatedData.search.split(' ')
+        let searchResult
+
+        if (search.length >= 1) {
+          searchResult = await ProgramList.query().where((query) => {
+            query
+              .where('name', 'like', `%${validatedData.search}%`)
+              .orWhere('name', 'like', `%${validatedData.search}`)
+              .orWhere('name', 'like', `${validatedData.search}%`)
+          })
+        }
+
+        return view.render('pages/programs/search', {
+          title: 'Результаты поиска',
+          searchResult,
+          searchStr: validatedData.search,
+        })
+      }
+    }
   }
 
-  public async searchInQuestion({ request, view }: HttpContextContract) {
-    const validateData = await request.validate(RequestSearchValidator)
+  public async searchInQuestion({ request, response, view, session }: HttpContextContract) {
+    const validatedData = await request.validate(RequestSearchValidator)
 
-    const search = validateData.search.split(' ')
-    let searchResult
-
-    if (search.length >= 1) {
-      searchResult = await Question.query()
-        .where((query) => {
-          query
-            .where('description_question', 'like', `%${validateData.search}%`)
-            .orWhere('description_question', 'like', `%${validateData.search}`)
-            .orWhere('description_question', 'like', `${validateData.search}%`)
-        })
+    for (const validatedDataKey in validatedData) {
+      if (!validatedData[validatedDataKey]) {
+        delete validatedData[validatedDataKey]
+      }
     }
 
-    return view.render('pages/articles/search', {
-      title: 'Результаты поиска',
-      searchResult,
-      searchStr: validateData.search
-    })
+    if (Object.keys(validatedData).length === 0) {
+      session.flash('dangermessage', `Введенно пустое значение в поле название.`)
+      response.redirect('back')
+    } else {
+      if (validatedData.hasOwnProperty('search')) {
+        const search = validatedData.search.split(' ')
+        let searchResult
+
+        if (search.length >= 1) {
+          searchResult = await Question.query().where((query) => {
+            query
+              .where('description_question', 'like', `%${validatedData.search}%`)
+              .orWhere('description_question', 'like', `%${validatedData.search}`)
+              .orWhere('description_question', 'like', `${validatedData.search}%`)
+          })
+        }
+
+        return view.render('pages/articles/search', {
+          title: 'Результаты поиска',
+          searchResult,
+          searchStr: validatedData.search,
+        })
+      }
+    }
   }
 }
