154daf130821ee9d41424a6f0464aec9a156c92c
«BolotnikovMS» <bolotnikovms@yandex.ru>
2022-04-25 18:26:18 +0500

    Creating an api controller

---
+++ app/Controllers/Http/Api/WorkingsDirectionsController.ts
@@ -0,0 +1,53 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+import WorkingDirection from 'App/Models/WorkingDirection'
+
+import {checkObjectProperty} from '../../../../utils/helpersFunc'
+import WorkingDirectionValidator from 'App/Validators/WorkingDirectionValidator'
+
+export default class WorkingsDirectionsController {
+  public async index ({request, logger}: HttpContextContract) {
+    const urlParams = request.qs()
+
+    if (!Object.keys(urlParams).length) {
+      logger.info('Receiving all data on directions on request.')
+
+      return WorkingDirection.query().preload('categories')
+    } else {
+      logger.info(`Getting all data about directions on request with parameters: limit ${urlParams.limit ? urlParams.limit : '--'}, offset ${urlParams.offset ? urlParams.offset : '--'}  .`)
+
+      return WorkingDirection
+        .query()
+        .preload('categories')
+        .limit(checkObjectProperty(urlParams, 'limit') ? +urlParams.limit : 100)
+        .offset(checkObjectProperty(urlParams, 'offset') ? +urlParams.offset : 0)
+    }
+  }
+
+  public async create ({}: HttpContextContract) {
+  }
+
+  public async store ({request, response, logger}: HttpContextContract) {
+   try {
+     const validatedData = await request.validate(WorkingDirectionValidator)
+
+     logger.info(`Adding a New Work Direction: ${validatedData.name}.`)
+     console.log(validatedData)
+   } catch (error) {
+     logger.error(`Error: ${error.messages.name}`)
+     response.badRequest(error.messages)
+   }
+  }
+
+  public async show ({}: HttpContextContract) {
+  }
+
+  public async edit ({}: HttpContextContract) {
+  }
+
+  public async update ({}: HttpContextContract) {
+  }
+
+  public async destroy ({}: HttpContextContract) {
+  }
+}
