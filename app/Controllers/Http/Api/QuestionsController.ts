29f50a0af166a0d4dfac74d26e99bae722dde495
«BolotnikovMS» <bolotnikovms@yandex.ru>
2022-05-03 16:36:23 +0500

    Added methods for getting and deleting records

--- app/Controllers/Http/Api/QuestionsController.ts
+++ app/Controllers/Http/Api/QuestionsController.ts
@@ -1,13 +1,37 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Question from 'App/Models/Question'
 
 export default class QuestionsController {
   public async index({}: HttpContextContract) {}
 
   public async store({}: HttpContextContract) {}
 
-  public async show({}: HttpContextContract) {}
+  public async show({ response, params, logger }: HttpContextContract) {
+    const question = await Question.find(params.idQuestion)
+
+    if (question) {
+      logger.info(`Question received: ${question}.`)
+      return response.send(question)
+    } else {
+      logger.error('The question you are trying to get does not exist...')
+      return response.status(404).json({error: 'The question you are trying to get does not exist...'})
+    }
+  }
 
   public async update({}: HttpContextContract) {}
 
-  public async destroy({}: HttpContextContract) {}
+  public async destroy({ response, params, logger }: HttpContextContract) {
+    const question = await Question.find(params.idQuestion)
+
+    if (question) {
+      logger.info(`Entry: ${question} removed.`)
+
+      await question.delete()
+
+      return response.ok({message: 'Entry removed.'})
+    } else {
+      logger.error(`Error: Not found.`)
+      return response.notFound({ error: 'Not found.' })
+    }
+  }
 }
